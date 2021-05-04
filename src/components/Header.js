d84ce21ac0d30edf6f9a2c49902e9f44e506dd05
syedalirazazaidi <46336938+syedalirazazaidi@users.noreply.github.com>
2021-05-04 15:24:38 +0500

    add HEADER

--- src/components/Header.js
+++ src/components/Header.js
@@ -30,6 +30,7 @@ function Header() {
           <span>SERIES</span>
         </a>
       </Navmenu>
+      <UserImg src="/images/mypic.jpg"></UserImg>
     </Nav>
   );
 }
@@ -42,21 +43,53 @@ const Nav = styled.nav`
   align-items: center;
   padding: 0 36px;
 `;
+
 const Logo = styled.img`
   width: 80px;
 `;
 const Navmenu = styled.div`
   display: flex;
+  flex: 1;
+  margin-left: 25px;
+  align-items: center;
   a {
     display: flex;
     align-items: center;
     padding: 0 12px;
+    cursor: pointer;
+
+    img {
+      height: 20px;
+    }
+    span {
+      font-size: 13px;
+      letter-spacing: 1.42px;
+      position: relative;
+      &:after {
+        content: "";
+        height: 2px;
+        background: white;
+        position: absolute;
+        left: 0;
+        right: 0;
+        bottom: -6px;
+        opacity: 0;
+        transform-origin: left center;
+        transition: all 250ms cubic-bezier(0.25, 0.46, 0.45, 0.94) 0s;
+        transform: scaleX(0);
+      }
+    }
+    &:hover {
+      span:after {
+        transform: scaleX(1);
+        opacity: 1;
+      }
+    }
   }
-  img {
-    height: 20px;
-  }
-  span {
-    font-size: 13px;
-    letter-spacing: 1.42px;
-  }
+`;
+const UserImg = styled.img`
+  width: 40px;
+  height: 40px;
+  border-radius: 50%;
+  cursor: pointer;
 `;
