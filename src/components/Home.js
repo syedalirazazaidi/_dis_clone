d43444fd19849b56bc0e9fb5af2c766ab32e8306
syedalirazazaidi <46336938+syedalirazazaidi@users.noreply.github.com>
2021-05-05 07:19:55 +0500

    add viewers

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,13 @@
 import React from "react";
 import styled from "styled-components";
 import ImgSlider from "./ImgSlider";
+import Viewer from "./Viewers";
 
 function Home() {
   return (
     <Container>
       <ImgSlider />
+      <Viewer />
     </Container>
   );
 }
